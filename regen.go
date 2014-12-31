785b5cdcfe1a47c5e0b8984cac07d7a24f6d0eeb
Zach Klippenstein <zach.klippenstein@gmail.com>
2014-12-31 09:47:25 -0800

    Pattern will be read from stdin if not passed on command line.

--- regen.go
+++ regen.go
@@ -20,9 +20,11 @@ import (
 	"flag"
 	"fmt"
 	"github.com/zach-klippenstein/goregen"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"path"
+	"strings"
 	"time"
 )
 
@@ -30,20 +32,26 @@ var NumberOfGenerations = flag.Int("n", 1, "number of strings to generate. A val
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [args] pattern\n", path.Base(os.Args[0]))
+		fmt.Fprintf(os.Stderr, "Usage: %s [args] [pattern]\n", path.Base(os.Args[0]))
 		fmt.Fprintln(os.Stderr, "Generates random strings from regular expressions.")
+		fmt.Fprintln(os.Stderr, "If no pattern is given on the command line, it will be read from stdin. If the last character is \\n, it will be removed.")
+		fmt.Fprintln(os.Stderr, "\nargs:")
 		flag.PrintDefaults()
 	}
 	flag.Parse()
 
-	if flag.NArg() != 1 {
-		fmt.Fprintln(os.Stderr, "error: must specify a pattern")
+	var pattern string
+
+	if flag.NArg() > 1 {
+		fmt.Fprintln(os.Stderr, "error: too many arguments")
 		flag.Usage()
+	} else if flag.NArg() == 1 {
+		pattern = flag.Arg(0)
+	} else {
+		pattern = ReadPatternFromStdin()
 	}
 
 	rand.Seed(time.Now().UTC().UnixNano())
-
-	pattern := flag.Arg(0)
 
 	generator, err := regen.NewGenerator(pattern, nil)
 	if err != nil {
@@ -54,4 +62,19 @@ func main() {
 	for i := 0; *NumberOfGenerations == 0 || i < *NumberOfGenerations; i++ {
 		fmt.Println(generator.Generate())
 	}
+}
+
+func ReadPatternFromStdin() string {
+	patternBytes, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error reading from stdin: %s\n", err)
+		os.Exit(1)
+	}
+	pattern := string(patternBytes)
+
+	// if entered interactively, there will be an extra \n at the end that
+	// is probably not intended to be part of the regex.
+	pattern = strings.TrimSuffix(pattern, "\n")
+
+	return pattern
 }
